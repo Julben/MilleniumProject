cae29504952051afcdba17f6ecdeae55f981c412
Juvito2003 <161456012+Juvito2003@users.noreply.github.com>
2024-05-07 13:18:23 +0200

    classcouleur

--- src/main/java/org/example/milleniumproject/model/Party.java
+++ src/main/java/org/example/milleniumproject/model/Party.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.example.milleniumproject.view.Menu;
 
+import static org.example.milleniumproject.model.ButtonColorChecker.checkAndChangeButtonColor;
 import static org.example.milleniumproject.model.ButtonPause.afficherRegles;
 import static org.example.milleniumproject.model.ButtonUtils.isNeighbourButton;
 
@@ -196,74 +197,7 @@ public class Party extends StackPane {
         }
     }
 
-    private void checkAndChangeButtonColor(String buttonLabel1, String buttonLabel2, String buttonLabel3, GridPane gridPane) {
-        // Variables pour stocker les URLs des boutons
-        String url1 = null;
-        String url2 = null;
-        String url3 = null;
 
-        // Obtenez tous les nœuds enfants du GridPane
-        ObservableList<Node> children = gridPane.getChildren();
-
-        // Parcourez chaque nœud pour obtenir les informations de chaque bouton
-        for (Node node : children) {
-            if (node instanceof Button) {
-                Button currentButton = (Button) node;
-                String buttonText = getButtonText(currentButton);
-                String imageUrl = getImageUrlFromButton(currentButton);
-
-                // Stockez les URLs des boutons
-                if (buttonText.equals(buttonLabel1)) {
-                    url1 = imageUrl;
-                } else if (buttonText.equals(buttonLabel2)) {
-                    url2 = imageUrl;
-                } else if (buttonText.equals(buttonLabel3)) {
-                    url3 = imageUrl;
-                }
-            }
-        }
-
-        // Vérifiez si les URLs des boutons sont identiques
-        if (url1 != null && url2 != null && url3 != null && url1.equals(url2) && url1.equals(url3)) {
-            // Parcourez à nouveau chaque nœud pour changer la couleur des boutons
-            for (Node node : children) {
-                if (node instanceof Button) {
-                    Button currentButton = (Button) node;
-                    String buttonText = getButtonText(currentButton);
-                    // Si le bouton est l'un des boutons spécifiés, changez sa couleur en rouge
-                    if (buttonText.equals(buttonLabel1) || buttonText.equals(buttonLabel2) || buttonText.equals(buttonLabel3)) {
-                        currentButton.setStyle("-fx-background-color: yellow;"+
-                                "-fx-background-radius: 50%; " + // Rendre les coins ronds
-                                "-fx-min-width: 65px; " + // Définir la largeur
-                                "-fx-min-height: 65px; " + // Définir la hauteur
-                                "-fx-max-width: 65px; " + // Limiter la largeur
-                                "-fx-max-height: 65px;");
-                    }
-                }
-            }
-        }
-    }
-
-    private String getImageUrlFromButton(Button button) {
-        // Vérifier si le bouton contient une image
-        if (button.getGraphic() instanceof ImageView) {
-            ImageView imageView = (ImageView) button.getGraphic();
-            Image image = imageView.getImage();
-
-            // Récupérer l'URL de l'image
-            String imageUrl = image.getUrl();
-            return imageUrl;
-        } else {
-            // Le bouton ne contient pas d'image
-            return null;
-        }
-    }
-
-    private String getButtonText(Button button) {
-        // Récupérer le texte du bouton
-        String buttonText = button.getText();
-        return buttonText;
-    }
 
     // Méthode pour gérer la sélection du bouton
     private void handleSelection(List<Button> buttons ,Button clickedButton) {
--- src/main/java/org/example/milleniumproject/model/PartyIA.java
+++ src/main/java/org/example/milleniumproject/model/PartyIA.java
@@ -21,11 +21,14 @@ import org.example.milleniumproject.view.Menu;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import static org.example.milleniumproject.model.ButtonColorChecker.checkAndChangeButtonColor;
 import static org.example.milleniumproject.model.ButtonPause.afficherRegles;
 import static org.example.milleniumproject.model.ButtonUtils.getNodeByRowColumnIndex;
 import static org.example.milleniumproject.model.ButtonUtils.isNeighbourButton;
 import javafx.animation.PauseTransition;
 import javafx.util.Duration;
+
 
 
 public class PartyIA extends StackPane {
@@ -275,57 +278,6 @@ public class PartyIA extends StackPane {
         }
     }
 
-    private void checkAndChangeButtonColor(String buttonLabel1, String buttonLabel2, String buttonLabel3, GridPane gridPane) {
-        // Variables pour stocker les URLs des boutons
-        String url1 = null;
-        String url2 = null;
-        String url3 = null;
-
-        // Obtenez tous les nœuds enfants du GridPane
-        ObservableList<Node> children = gridPane.getChildren();
-
-        // Parcourez chaque nœud pour obtenir les informations de chaque bouton
-        for (Node node : children) {
-            if (node instanceof Button) {
-                Button currentButton = (Button) node;
-                String buttonText = getButtonText(currentButton);
-                String imageUrl = getImageUrlFromButton(currentButton);
-
-                // Stockez les URLs des boutons
-                if (buttonText.equals(buttonLabel1)) {
-                    url1 = imageUrl;
-                } else if (buttonText.equals(buttonLabel2)) {
-                    url2 = imageUrl;
-                } else if (buttonText.equals(buttonLabel3)) {
-                    url3 = imageUrl;
-                }
-            }
-        }
-
-        // Vérifiez si les URLs des boutons sont identiques
-        if (url1 != null && url2 != null && url3 != null && url1.equals(url2) && url1.equals(url3)) {
-            // Parcourez à nouveau chaque nœud pour changer la couleur des boutons
-            for (Node node : children) {
-                if (node instanceof Button) {
-                    Button currentButton = (Button) node;
-                    String buttonText = getButtonText(currentButton);
-                    // Si le bouton est l'un des boutons spécifiés, changez sa couleur en rouge
-                    if (buttonText.equals(buttonLabel1) || buttonText.equals(buttonLabel2) || buttonText.equals(buttonLabel3)) {
-                        currentButton.setStyle("-fx-background-color: yellow;"+
-                                "-fx-background-radius: 50%; " + // Rendre les coins ronds
-                                "-fx-min-width: 65px; " + // Définir la largeur
-                                "-fx-min-height: 65px; " + // Définir la hauteur
-                                "-fx-max-width: 65px; " + // Limiter la largeur
-                                "-fx-max-height: 65px;");
-                    }
-                }
-            }
-        }
-    }
-
-
-
-
     // Méthode pour gérer la sélection du bouton
     private void handleSelection(List<Button> buttons ,Button clickedButton) {
         if (selectedButton == null) {
@@ -369,26 +321,7 @@ public class PartyIA extends StackPane {
 
 
 
-    private String getImageUrlFromButton(Button button) {
-        // Vérifier si le bouton contient une image
-        if (button.getGraphic() instanceof ImageView) {
-            ImageView imageView = (ImageView) button.getGraphic();
-            Image image = imageView.getImage();
 
-            // Récupérer l'URL de l'image
-            String imageUrl = image.getUrl();
-            return imageUrl;
-        } else {
-            // Le bouton ne contient pas d'image
-            return null;
-        }
-    }
-
-    private String getButtonText(Button button) {
-        // Récupérer le texte du bouton
-        String buttonText = button.getText();
-        return buttonText;
-    }
 
 
 
