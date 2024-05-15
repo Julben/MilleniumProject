d7c6d37c716471cd289b5ffbf57b6d6ea1afb14c
Julben <160669519+Julben@users.noreply.github.com>
2024-05-15 14:58:26 +0200

    ajout label

--- src/main/java/org/example/milleniumproject/model/Chargement.java
+++ src/main/java/org/example/milleniumproject/model/Chargement.java
@@ -4,7 +4,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -13,7 +13,6 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.File;
-
 
 public class Chargement extends StackPane {
 
@@ -56,49 +55,44 @@ public class Chargement extends StackPane {
                 "-fx-border-color: white; " + // Couleur de contour noir
                 "-fx-border-width: 3px;"); // Épaisseur du contour
 
-
         Button lancerPartieButton = new Button("Lancer la partie");
         lancerPartieButton.setFont(Font.font("Cardo", FontWeight.BOLD, 20));
         lancerPartieButton.setStyle("-fx-background-color: white; -fx-text-fill: black;");
 
-        // Ajouter un événement pour gérer le clic sur le bouton "Lancer la partie"
-        lancerPartieButton.setOnAction(e -> {
-             String fileChoisi = comboBox.getValue();
-            if (fileChoisi != null) {
+        Label messageLabel = new Label();
+        messageLabel.setFont(Font.font("Cardo", FontWeight.NORMAL, 20));
+        messageLabel.setStyle("-fx-text-fill: white;");
+        messageLabel.setPrefWidth(250);
+        messageLabel.setPrefHeight(30);
 
+
+        lancerPartieButton.setOnAction(e -> {
+            String fileChoisi = comboBox.getValue();
+            if (fileChoisi != null) {
                 LoadPartyCall loadPartyCall = new LoadPartyCall(primaryStage, fileChoisi);
                 primaryStage.getScene().equals(loadPartyCall);
             } else {
                 // Aucun fichier sélectionné dans la ComboBox
-                System.err.println("Aucun fichier sélectionné.");
+                messageLabel.setText("Aucun fichier sélectionné.");
             }
-
         });
 
-
-
         // Créer un VBox pour organiser les éléments verticalement
-        VBox vbox1 = new VBox(20); // Espacement de 10 entre les éléments
-        vbox1.getChildren().addAll(comboBox, lancerPartieButton); // Ajouter le ComboBox et le bouton au VBox
-
+        VBox vbox1 = new VBox(20); // Espacement de 20 entre les éléments
+        vbox1.getChildren().addAll(messageLabel, comboBox, lancerPartieButton); // Ajouter le label, le ComboBox, et le bouton au VBox
         vbox1.setAlignment(Pos.CENTER_RIGHT);
-
 
         VBox vbox2 = new VBox(retourButton);
         vbox2.setAlignment(Pos.TOP_LEFT);
 
         // Créer une HBox pour placer les deux VBox horizontalement
-        HBox hbox = new HBox(0.35*Constant.screenWidth);
+        HBox hbox = new HBox(0.35 * Constant.screenWidth);
         hbox.setAlignment(Pos.CENTER);
         hbox.getChildren().addAll(vbox2, vbox1);
 
         // Ajouter la HBox à la StackPane
         getChildren().add(hbox);
-        HBox.setMargin(vbox1, new Insets(0.1*Constant.screenHeight,0.42*Constant.screenWidth , 0, 0));
+        HBox.setMargin(vbox1, new Insets(0.1 * Constant.screenHeight, 0.42 * Constant.screenWidth, 0, 0));
         HBox.setMargin(vbox2, new Insets(0, 0, 0, 0));
-
     }
-
-
 }
-
